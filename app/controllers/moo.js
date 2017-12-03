eb215af344a7be40fbe0e55f4ff224d1228c83fc
Arthur Shau <ashau@usc.edu>
2017-12-02 23:34:38 -0800

    changed url

--- app/controllers/moo.js
+++ app/controllers/moo.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	actions: {
     triggerMoo() {
-      let url = 'http://localhost:3000/moo'
+      let url = 'http://moo.pf-n.co/moo'
       $.post(url, (data) => {
       	console.log('moo\'d')
       })
