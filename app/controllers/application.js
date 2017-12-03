e94aeb6443806bef051a0abe2bbe5578469da4d5
Arthur Shau <ashau@usc.edu>
2017-12-03 00:20:26 -0800

    message

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,5 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  actions: {
-    searchForSubreddit() {
-      let subreddit = this.get('subreddit');
-
-      this.transitionToRoute('subreddits', subreddit);
-    }
-  }
+	
 });
--- app/controllers/moo.js
+++ app/controllers/moo.js
@@ -3,8 +3,9 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	actions: {
     triggerMoo() {
+      let message = {message: this.get('message')}; 
       let url = 'http://moo.pf-n.co/moo'
-      $.post(url, (data) => {
+      $.post(url, message, (data) => {
       	console.log('moo\'d')
       })
     }
